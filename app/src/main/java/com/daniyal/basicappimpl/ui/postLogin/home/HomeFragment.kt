99acb1c5661dae1aae6809798628b503175d781a
zbook <daniyal.moeen@express.com.pk>
2020-12-06 02:06:14 -0800

    Code Refactored

--- app/src/main/java/com/daniyal/basicappimpl/ui/postLogin/home/HomeFragment.kt
+++ app/src/main/java/com/daniyal/basicappimpl/ui/postLogin/home/HomeFragment.kt
@@ -7,7 +7,6 @@ import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.viewModels
 import com.daniyal.basicappimpl.databinding.FragmentHomeBinding
 import com.daniyal.basicappimpl.ui.base.BaseFragment
-import com.daniyal.basicappimpl.ui.postLogin.home.adapters.HomePagerAdapter
 import com.daniyal.basicappimpl.ui.postLogin.home.viewmodels.HomeViewModel
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
@@ -23,17 +22,16 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        initViewPager()
     }
 
-    private fun initViewPager() {
-        binding.pager.adapter = HomePagerAdapter(this)
-        TabLayoutMediator(
-            binding.tabLayout, binding.pager
-        ) { tab: TabLayout.Tab, position: Int ->
-            val tabDetail = homeViewModel.tabDetails[position]
-            tab.text = getString(tabDetail.first)
-            tab.icon = ResourcesCompat.getDrawable(resources, tabDetail.second, null)
-        }.attach()
-    }
+//    private fun initViewPager() {
+//        binding.pager.adapter = HomePagerAdapter(this)
+//        TabLayoutMediator(
+//            binding.tabLayout, binding.pager
+//        ) { tab: TabLayout.Tab, position: Int ->
+//            val tabDetail = homeViewModel.tabDetails[position]
+//            tab.text = getString(tabDetail.first)
+//            tab.icon = ResourcesCompat.getDrawable(resources, tabDetail.second, null)
+//        }.attach()
+//    }
 }
