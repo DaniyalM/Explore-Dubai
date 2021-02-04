ea4c7a7c03b379f76862b35e8717c3e62f67c6b2
zbook <daniyal.moeen@express.com.pk>
2021-02-03 19:27:06 -0800

    Api Key has been updated

--- app/src/main/java/com/app/dubaiculture/ui/postLogin/explore/ExploreFragment.kt
+++ app/src/main/java/com/app/dubaiculture/ui/postLogin/explore/ExploreFragment.kt
@@ -36,7 +36,7 @@ class ExploreFragment : BaseFragment<FragmentExploreBinding>() {
         explore = ExploreRecyclerAsyncAdapter(activity)
         subscribeToObservable()
         setUpRecyclerView()
-        createTestItems()
+
     }
 
     private fun setUpRecyclerView() {
@@ -60,103 +60,5 @@ class ExploreFragment : BaseFragment<FragmentExploreBinding>() {
 
     }
 
-
-    private fun createAttractionItems(): List<Attraction> = mutableListOf<Attraction>().apply {
-
-        repeat((1..70).count()) {
-            add(
-                Attraction(
-                    it - 1,
-                    "dubai culture ${it}",
-                    "https://cdn-sharing.adobecc.com/id/urn:aaid:sc:US:a8b582cb-91d1-4561-b05f-cfe1c0e7b414;version=0?component_id=a46d108d-0cd1-4619-86d9-53e449a87c1e&api_key=CometServer1&access_token=1611608278_urn%3Aaaid%3Asc%3AUS%3Aa8b582cb-91d1-4561-b05f-cfe1c0e7b414%3Bpublic_827967f49e41aad27f7dd2bb859c4045dc9c846e"
-                )
-            )
-        }
-    }
-    private fun createUpComingItems(): List<UpComingEvents> = mutableListOf<UpComingEvents>().apply {
-
-            repeat((1..70).count()) {
-                add(
-                    UpComingEvents(
-                        it - 1,
-                        "dubai culture ${it}",
-                        "https://cdn-sharing.adobecc.com/id/urn:aaid:sc:US:a8b582cb-91d1-4561-b05f-cfe1c0e7b414;version=0?component_id=a46d108d-0cd1-4619-86d9-53e449a87c1e&api_key=CometServer1&access_token=1611608278_urn%3Aaaid%3Asc%3AUS%3Aa8b582cb-91d1-4561-b05f-cfe1c0e7b414%3Bpublic_827967f49e41aad27f7dd2bb859c4045dc9c846e",
-                        "",
-                        "",
-                        "",
-                        "",
-                        "",
-                        false,
-                        ""
-                    )
-                )
-            }
-        }
-    private fun createMustSeeItems(): List<MustSee> = mutableListOf<MustSee>().apply {
-
-        repeat((1..70).count()) {
-            add(
-
-                MustSee(
-                    it - 1,
-                    "dubai culture ${it}",
-                    "https://cdn-sharing.adobecc.com/id/urn:aaid:sc:US:a8b582cb-91d1-4561-b05f-cfe1c0e7b414;version=0?component_id=a46d108d-0cd1-4619-86d9-53e449a87c1e&api_key=CometServer1&access_token=1611608278_urn%3Aaaid%3Asc%3AUS%3Aa8b582cb-91d1-4561-b05f-cfe1c0e7b414%3Bpublic_827967f49e41aad27f7dd2bb859c4045dc9c846e",
-                    false,
-                    "",
-
-                    )
-            )
-        }
-    }
-
-    private fun createPopularServiceItems(): List<PopularServices> = mutableListOf<PopularServices>().apply {
-
-        repeat((1..70).count()) {
-            add(
-
-                PopularServices(
-                    it - 1,
-                    "dubai culture ${it}",
-                    "https://cdn-sharing.adobecc.com/id/urn:aaid:sc:US:a8b582cb-91d1-4561-b05f-cfe1c0e7b414;version=0?component_id=a46d108d-0cd1-4619-86d9-53e449a87c1e&api_key=CometServer1&access_token=1611608278_urn%3Aaaid%3Asc%3AUS%3Aa8b582cb-91d1-4561-b05f-cfe1c0e7b414%3Bpublic_827967f49e41aad27f7dd2bb859c4045dc9c846e",
-                )
-            )
-        }
-    }
-
-    private fun createLatestNewsItems(): List<LatestNews> = mutableListOf<LatestNews>().apply {
-
-        repeat((1..70).count()) {
-            add(
-
-                LatestNews(
-                    it - 1,
-                    "dubai culture ${it}",
-                    "",
-                    "https://cdn-sharing.adobecc.com/id/urn:aaid:sc:US:a8b582cb-91d1-4561-b05f-cfe1c0e7b414;version=0?component_id=a46d108d-0cd1-4619-86d9-53e449a87c1e&api_key=CometServer1&access_token=1611608278_urn%3Aaaid%3Asc%3AUS%3Aa8b582cb-91d1-4561-b05f-cfe1c0e7b414%3Bpublic_827967f49e41aad27f7dd2bb859c4045dc9c846e",
-                    ""
-                )
-            )
-        }
-    }
-    private fun createTestItems(): List<TestItem> = mutableListOf<TestItem>().apply {
-        repeat((1..3).count()) {
-
-            add(
-                TestItem(
-                    it - 1,
-                    "description $it",
-                    it,
-                    "title $it",
-                    it,
-                    it,
-                    createAttractionItems(),
-                    createUpComingItems(),
-                    createMustSeeItems(),
-                    createPopularServiceItems(),
-                    createLatestNewsItems()
-                )
-            )
-        }
-    }
 
 }
