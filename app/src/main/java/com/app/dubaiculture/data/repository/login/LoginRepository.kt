49badc13c59a1f6701201ff539031bf7cb1f2392
m__ammar <m.ammar.appxone@gmail.com>
2021-02-07 13:55:40 +0500

    working on Login API integration

---
+++ app/src/main/java/com/app/dubaiculture/data/repository/login/LoginRepository.kt
@@ -0,0 +1,26 @@
+package com.app.dubaiculture.data.repository.login
+
+import com.app.dubaiculture.data.Result
+import com.app.dubaiculture.data.repository.base.BaseRepository
+import com.app.dubaiculture.data.repository.login.remote.LoginRDS
+import com.app.dubaiculture.data.repository.login.remote.request.LoginRequest
+import com.app.dubaiculture.data.repository.login.remote.response.LoginResponse
+import com.app.dubaiculture.data.repository.registeration.remote.mapper.transform
+import com.app.neomads.data.repository.registration.remote.request.register.RegistrationRequest
+import javax.inject.Inject
+
+class LoginRepository @Inject constructor(private val loginRDS: LoginRDS):BaseRepository() {
+        suspend fun login(loginRequest: LoginRequest): Result<LoginResponse> {
+            return when(val resultRDS = loginRDS.login(transform(loginRequest))){
+                    is Result.Success->{
+                        Result.Success(resultRDS.value)
+                    }
+                    is Result.Error->{
+                        Result.Error(resultRDS.exception)
+                    }
+                    is Result.Failure ->{
+                        Result.Failure(resultRDS.isNetWorkError, resultRDS.errorCode, resultRDS.errorBody)
+                    }
+            }
+        }
+}
