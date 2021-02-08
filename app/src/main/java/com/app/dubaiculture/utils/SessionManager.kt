866c666a58eedbf46a53dc196d7ec5926d95fd86
zbook <daniyal.moeen@express.com.pk>
2021-02-08 15:44:02 -0800

    Lottie Animation Integrated

--- app/src/main/java/com/app/dubaiculture/utils/SessionManager.kt
+++ app/src/main/java/com/app/dubaiculture/utils/SessionManager.kt
@@ -2,4 +2,6 @@ package com.app.dubaiculture.utils
 
 import javax.inject.Inject
 
-class SessionManager @Inject constructor() { var authorizationToken: String = "auth_token1234567890987654321" }
+class SessionManager @Inject constructor() {
+    var authorizationToken: String = "auth_token1234567890987654321"
+}
