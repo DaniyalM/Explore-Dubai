1d32ca097cff1cd2dd118d35c0d69fc995dcd63f
m__ammar <m.ammar.appxone@gmail.com>
2021-02-08 12:35:37 +0500

    Login API Integrated.

--- app/src/main/java/com/app/dubaiculture/data/repository/login/mapper/LoginMapper.kt
+++ app/src/main/java/com/app/dubaiculture/data/repository/login/mapper/LoginMapper.kt
@@ -1,19 +1,13 @@
-package com.app.dubaiculture.data.repository.registeration.remote.mapper
-
 import com.app.dubaiculture.data.repository.login.remote.request.LoginRequest
 import com.app.dubaiculture.data.repository.login.remote.request.LoginRequestDTO
-import com.app.dubaiculture.data.repository.login.remote.response.LoginResponseDTO
-import com.app.dubaiculture.data.repository.registeration.remote.request.confirmOTP.ConfirmOTPRequest
-import com.app.dubaiculture.data.repository.registeration.remote.request.confirmOTP.ConfirmOTPRequestDTO
-import com.app.neomads.data.repository.registration.remote.request.register.RegistrationRequest
-import com.app.neomads.data.repository.registration.remote.request.register.RegistrationRequestDTO
+
+
 
 fun transform(loginRequest: LoginRequest): LoginRequestDTO {
     return LoginRequestDTO(
         Email = loginRequest.email,
         Password = loginRequest.password
     )
+
 }
-
-
 
