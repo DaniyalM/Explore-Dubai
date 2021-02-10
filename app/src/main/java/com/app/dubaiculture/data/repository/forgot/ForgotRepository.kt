d854ef1bbe8042bbfa53e53d8c4052d5417692ee
m__ammar <m.ammar.appxone@gmail.com>
2021-02-10 13:35:52 +0500

    working on forgot API

--- app/src/main/java/com/app/dubaiculture/data/repository/forgot/ForgotRepository.kt
+++ app/src/main/java/com/app/dubaiculture/data/repository/forgot/ForgotRepository.kt
@@ -1,14 +1,22 @@
 package com.app.dubaiculture.data.repository.forgot
 
+import com.app.dubaiculture.data.Result
+import com.app.dubaiculture.data.repository.base.BaseRepository
+import com.app.dubaiculture.data.repository.forgot.mapper.transform
+import com.app.dubaiculture.data.repository.forgot.remote.ForgotRDS
 import com.app.dubaiculture.data.repository.forgot.remote.request.ForgotRequest
 import com.app.dubaiculture.data.repository.forgot.remote.response.ForgotResponse
-import com.app.dubaiculture.data.repository.forgot.remote.response.ForgotResponseDTO
-import com.app.dubaiculture.data.repository.login.remote.request.LoginRequestDTO
 import com.app.dubaiculture.data.repository.login.remote.response.LoginResponse
-import retrofit2.http.Body
-import retrofit2.http.POST
+import javax.inject.Inject
 
-interface ForgotRepository {
-    @POST("Auth/ForgotPassword")
-    suspend fun forgot(@Body forgotResponseDTO: ForgotResponseDTO): ForgotResponse
+class ForgotRepository @Inject constructor(private val forgotRDS: ForgotRDS): BaseRepository() {
+    suspend fun forgot(forgotRequest: ForgotRequest): Result<ForgotResponse> {
+        return when(val resultRDS = forgotRDS.forgotEmail(transform(forgotRequest))){
+            is Result.Success->{
+                Result.Success(resultRDS.value)
+            }
+            is Result.Error->resultRDS
+            is Result.Failure ->resultRDS
+        }
+    }
 }
--- app/src/main/java/com/app/dubaiculture/ui/preLogin/forgot/viewModel/ForgotViewModel.kt
+++ app/src/main/java/com/app/dubaiculture/ui/preLogin/forgot/viewModel/ForgotViewModel.kt
@@ -1,9 +1,62 @@
 package com.app.dubaiculture.ui.preLogin.forgot.viewModel
 
 import android.app.Application
+import androidx.databinding.ObservableBoolean
+import androidx.databinding.ObservableField
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.app.dubaiculture.data.Result
+import com.app.dubaiculture.data.repository.forgot.ForgotRepository
+import com.app.dubaiculture.data.repository.forgot.remote.request.ForgotRequest
 import com.app.dubaiculture.ui.base.BaseViewModel
+import com.app.dubaiculture.utils.AuthUtils
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
-class ForgotViewModel @ViewModelInject constructor(application: Application): BaseViewModel(application) {
+class ForgotViewModel @ViewModelInject constructor(private val forgotRepository: ForgotRepository, application: Application): BaseViewModel(application) {
 
+    var email: ObservableField<String> = ObservableField("")
+    //boolean for visibility of hide and gone of img error
+    val isEmail = MutableLiveData<Boolean?>(true)
+     // Field Error Messages
+     val emailError = MutableLiveData<String?>()
+    // for button enable and disable
+    var btnEnabled: ObservableBoolean = ObservableBoolean(false)
+
+
+    // for button enable disable
+    fun enableButton() {
+        btnEnabled.set(
+            AuthUtils.isEmailValid(email.get().toString().trim())
+        )
+    }
+
+    fun forgotEmail(){
+        viewModelScope.launch {
+            showLoader(true)
+            ForgotRequest(
+                email = "ammar@gmail.com"
+            ).let {
+                when(val result =forgotRepository.forgot(it)){
+                    is Result.Success->{
+                        if(result.value.succeeded){
+                            showLoader(false)
+                            showToast(result.value.forgotResponseDTO.message.toString())
+                        }else{
+                            showToast(result.value.errorMessage)
+                        }
+                    }
+                    is Result.Error->{
+                        Timber.e(result.exception)
+
+                    }
+                    is Result.Failure->{
+                        Timber.e(result.errorCode.toString())
+                    }
+                }
+            }
+            showLoader(false)
+        }
+    }
 }
