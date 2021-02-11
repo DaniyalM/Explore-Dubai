07261cf9121492874d3690b345af267f08783915
Daniyal Moeen <dmoeen@wewanttraffic.pk>
2021-02-11 17:03:02 +0500

    Interceptor work

--- app/src/main/java/com/app/dubaiculture/utils/SessionManager.kt
+++ app/src/main/java/com/app/dubaiculture/utils/SessionManager.kt
@@ -1,17 +1,19 @@
 package com.app.dubaiculture.utils
 
-import android.util.Log
 import com.app.dubaiculture.data.repository.user.UserRepository
+import dagger.Lazy
+import timber.log.Timber
+import java.util.*
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 import javax.inject.Singleton
-import dagger.Lazy
 
 
 @Singleton
 class SessionManager @Inject constructor(private val userRepository: Lazy<UserRepository>) {
 //    var authorizationToken: String = "auth_token1234567890987654321"
 
-    suspend fun getGuestToken(): Pair<Boolean,String>?{
+    suspend fun getGuestToken(): Pair<Boolean, String>?{
         val userRepo=userRepository.get()
         return userRepo.guestToken("667bfd7b08bb71d2")
     }
@@ -23,7 +25,18 @@ class SessionManager @Inject constructor(private val userRepository: Lazy<UserRe
             return Pair(false, "")
         } else {
             var token = user.refreshToken
-            var expiry=user.expireIn
+            var isWantTokenRefresh=false
+            val current_time: Long = Calendar.getInstance().getTimeInMillis()
+            Timber.e("Current Time ${current_time}")
+            user.expireIn?.let {
+                var expiry_time=  TimeUnit.SECONDS.toMillis(it.toLong());
+
+
+
+//                val final_time
+
+            }
+
             //if token is expired
 
 
