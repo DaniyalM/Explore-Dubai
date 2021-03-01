ce4000f25b98741cfc22c6b93f414986f1c6ed2f
m__ammar <m.ammar.appxone@gmail.com>
2021-03-01 13:07:39 +0500

    Uae Pass ar image

--- app/src/main/java/com/app/dubaiculture/ui/postLogin/attractions/detail/AttractionDetailFragment.kt
+++ app/src/main/java/com/app/dubaiculture/ui/postLogin/attractions/detail/AttractionDetailFragment.kt
@@ -1,4 +1,27 @@
 package com.app.dubaiculture.ui.postLogin.attractions.detail
 
-class AttractionDetailFragment {
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.app.dubaiculture.R
+import com.app.dubaiculture.databinding.FragmentAttractionDetailBinding
+import com.app.dubaiculture.ui.base.BaseFragment
+
+class AttractionDetailFragment : BaseFragment<FragmentAttractionDetailBinding>() {
+
+
+
+    override fun getFragmentBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+    ) = FragmentAttractionDetailBinding.inflate(inflater,container,false)
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+    }
+
 }
