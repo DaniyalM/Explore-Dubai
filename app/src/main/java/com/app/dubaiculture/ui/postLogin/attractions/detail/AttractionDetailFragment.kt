b6b6fbd3d33241d9d46a3948888b40c2fffe33f9
zbook <daniyal.moeen@express.com.pk>
2021-03-03 17:58:07 -0800

    Detail Collapsing Work Has been done

--- app/src/main/java/com/app/dubaiculture/ui/postLogin/attractions/detail/AttractionDetailFragment.kt
+++ app/src/main/java/com/app/dubaiculture/ui/postLogin/attractions/detail/AttractionDetailFragment.kt
@@ -1,16 +1,23 @@
 package com.app.dubaiculture.ui.postLogin.attractions.detail
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import com.app.dubaiculture.databinding.FragmentAttractionDetailBinding
 import com.app.dubaiculture.ui.base.BaseFragment
+import com.google.android.material.appbar.AppBarLayout
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.toolbar_snippet.view.*
 
+
 @AndroidEntryPoint
 class AttractionDetailFragment : BaseFragment<FragmentAttractionDetailBinding>() {
+    private var behavior: AppBarLayout.Behavior? = null
+    private var coordinatorLayout: CoordinatorLayout.LayoutParams? = null
+
     override fun getFragmentBinding(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -18,8 +25,34 @@ class AttractionDetailFragment : BaseFragment<FragmentAttractionDetailBinding>()
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        binding.root.uniqueToolbar.visibility=View.GONE
-        binding.root.detailToolbar.visibility=View.VISIBLE
+        Handler(Looper.getMainLooper()).postDelayed({
+            collapseAppbar()
+
+        },3000)
+        Handler(Looper.getMainLooper()).postDelayed({
+            expandAppbar()
+
+        },4000)
+    }
+
+
+    private fun collapseAppbar() {
+        binding.appbarAttractionDetail.setExpanded(false)
+//        coordinatorLayout = binding.appbarAttractionDetail.layoutParams as CoordinatorLayout.LayoutParams?
+//        behavior= coordinatorLayout?.behavior as AppBarLayout.Behavior?
+//
+//        if (behavior != null) {
+//            behavior!!.onNestedFling(binding.appCoordinatorAttractionDetail, binding.appbarAttractionDetail, binding.root, 0f, 10000f, true)
+//        }
+    }
+
+    private fun expandAppbar() {
+        binding.appbarAttractionDetail.setExpanded(true)
+//        coordinatorLayout = binding.appbarAttractionDetail.layoutParams as CoordinatorLayout.LayoutParams?
+//        behavior= coordinatorLayout?.behavior as AppBarLayout.Behavior?
+//        if(behavior!=null) {
+//      }
+
 
     }
 }
