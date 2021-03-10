1fa0c4568c2e9deacf8915ee003e832ffa2eb1bf
zbook <daniyal.moeen@express.com.pk>
2021-03-09 18:48:06 -0800

    Issue has been resolved

--- app/src/main/java/com/app/dubaiculture/data/repository/event/local/models/EventHomeListing.kt
+++ app/src/main/java/com/app/dubaiculture/data/repository/event/local/models/EventHomeListing.kt
@@ -2,6 +2,7 @@ package com.app.dubaiculture.data.repository.event.local.models
 
 
 data class EventHomeListing(
+    var title: String? = null,
     var category: String? = null,
     var events: List<Events>,
 )
--- app/src/main/java/com/app/dubaiculture/ui/postLogin/events/EventsFragment.kt
+++ app/src/main/java/com/app/dubaiculture/ui/postLogin/events/EventsFragment.kt
@@ -5,24 +5,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.app.dubaiculture.data.repository.event.local.models.EventHomeListing
+import com.app.dubaiculture.data.repository.event.local.models.Events
 import com.app.dubaiculture.databinding.FragmentEventsBinding
 import com.app.dubaiculture.ui.base.BaseFragment
 import com.app.dubaiculture.ui.postLogin.events.adapters.EventRecyclerAsyncAdapter
-import com.app.dubaiculture.ui.postLogin.explore.adapters.ExploreRecyclerAsyncAdapter
 import com.bumptech.glide.RequestManager
 import dagger.hilt.android.AndroidEntryPoint
 import jp.wasabeef.recyclerview.animators.SlideInLeftAnimator
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class EventsFragment:BaseFragment<FragmentEventsBinding>() {
+class EventsFragment : BaseFragment<FragmentEventsBinding>() {
     private lateinit var event: EventRecyclerAsyncAdapter
+
     @Inject
     lateinit var glide: RequestManager
     override fun getFragmentBinding(
         inflater: LayoutInflater,
-        container: ViewGroup?
-    )= FragmentEventsBinding.inflate(inflater,container,false)
+        container: ViewGroup?,
+    ) = FragmentEventsBinding.inflate(inflater, container, false)
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
@@ -40,9 +42,65 @@ class EventsFragment:BaseFragment<FragmentEventsBinding>() {
             visibility = View.VISIBLE
             layoutManager = LinearLayoutManager(activity)
             adapter = event
-          //  event.provideGlideInstance(glide)
+            //  event.provideGlideInstance(glide)
             this.itemAnimator = SlideInLeftAnimator()
         }
+        event.items = createTestItems()
 
     }
+
+
+    private fun createTestItems(): List<EventHomeListing> =
+        mutableListOf<EventHomeListing>().apply {
+
+
+            repeat((1..2).count()) {
+                when (it % 2){
+                    0->{
+                        add(
+                            EventHomeListing(
+                                title = "FeatureEvents",
+                                category = "FeatureEvents",
+                                events = createAttractionItems()
+                            )
+                        )
+                    }
+                    else->{
+                        add(
+                            EventHomeListing(
+                                title = "More Events",
+                                category = "MoreEvents",
+                                events = createAttractionItems()
+                            )
+                        )
+                    }
+                }
+
+
+            }
+        }
+
+    private fun createAttractionItems(): ArrayList<Events> = mutableListOf<Events>().apply {
+
+
+        repeat((1..4).count()) {
+
+            add(
+                Events(
+                    id = it.toString(),
+                    title = "Title $it",
+                    category = "Category $it",
+                    fromDate = "18",
+                    fromMonthYear = "Mar, 21",
+                    fromTime = "201$it",
+                    fromDay = "1$it",
+                    toDate = "$it",
+                    toMonthYear = "Mar, 21",
+                    toTime = "202$it",
+                    toDay = "2$it",
+                    type ="Free"
+                )
+            )
+        }
+    } as ArrayList<Events>
 }
