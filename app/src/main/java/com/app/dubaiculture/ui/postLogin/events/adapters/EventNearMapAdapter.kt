29ce9d3e574eb20ab7100d034cef077ee6134f25
m__ammar <m.ammar.appxone@gmail.com>
2021-03-12 14:49:05 +0500

    Near by Map set dummy data

---
+++ app/src/main/java/com/app/dubaiculture/ui/postLogin/events/adapters/EventNearMapAdapter.kt
@@ -0,0 +1,71 @@
+package com.app.dubaiculture.ui.postLogin.events.adapters
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.app.dubaiculture.R
+import com.app.dubaiculture.data.repository.event.local.models.Events
+import com.app.dubaiculture.databinding.EventNearItemsBinding
+import com.app.dubaiculture.utils.AsyncCell
+
+class EventNearMapAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private val diffCallback = object : DiffUtil.ItemCallback<Events>() {
+        override fun areItemsTheSame(oldItem: Events, newItem: Events): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Events, newItem: Events): Boolean {
+            return oldItem.hashCode() == newItem.hashCode()
+        }
+    }
+    private val differ = AsyncListDiffer(this, diffCallback)
+
+    var events: List<Events>
+        get() = differ.currentList
+        set(value) {
+            differ.submitList(value)
+        }
+
+    inner class EventNearMapViewHolder(view: ViewGroup) : RecyclerView.ViewHolder(view)
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return EventNearMapViewHolder(EventsListItemCell(parent.context).apply { inflate() })
+
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        setUpEventNearMapViewHolder(holder = holder as EventNearMapAdapter.EventNearMapViewHolder,
+            position)
+    }
+
+    override fun getItemCount() = events.size
+
+    //Data Binding
+    private inner class EventsListItemCell(context: Context) : AsyncCell(context) {
+        var binding: EventNearItemsBinding? = null
+        override val layoutId = R.layout.event_near_items
+        override fun createDataBindingView(view: View): View? {
+            binding = EventNearItemsBinding.bind(view)
+            return view.rootView
+        }
+    }
+
+    private fun setUpEventNearMapViewHolder(
+        holder: EventNearMapAdapter.EventNearMapViewHolder,
+        position: Int,
+    ) {
+        (holder.itemView as EventNearMapAdapter.EventsListItemCell).bindWhenInflated {
+            holder.itemView.binding?.let {
+                try {
+                    it.events = events[position]
+                } catch (ex: IndexOutOfBoundsException) {
+                    print(ex.stackTrace)
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/app/dubaiculture/ui/postLogin/events/map/EventNearMapFragment.kt
+++ app/src/main/java/com/app/dubaiculture/ui/postLogin/events/map/EventNearMapFragment.kt
@@ -5,23 +5,52 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.app.dubaiculture.R
+import com.app.dubaiculture.data.repository.event.local.models.Events
+import com.app.dubaiculture.databinding.FragmentEventNearMapBinding
+import com.app.dubaiculture.ui.base.BaseFragment
+import com.app.dubaiculture.ui.postLogin.events.adapters.EventNearMapAdapter
 
 
-class EventNearMapFragment : Fragment() {
+class EventNearMapFragment : BaseFragment<FragmentEventNearMapBinding>() {
+    lateinit var eventNearAdapter : EventNearMapAdapter
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun getFragmentBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+    )= FragmentEventNearMapBinding.inflate(inflater,container,false)
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        arguments?.let {
+            // get near event model
+        }
+        rvSetUp()
+
+    }
+    private fun rvSetUp(){
+        eventNearAdapter = EventNearMapAdapter()
+        binding.rvNearEvent.apply {
+            layoutManager = LinearLayoutManager(context,LinearLayoutManager.HORIZONTAL,false)
+            adapter = eventNearAdapter
+        }
+        eventNearAdapter.events = createTestItems()
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_event_near_map, container, false)
-    }
+    private fun createTestItems(): List<Events> =
+        mutableListOf<Events>().apply {
+            repeat((1..4).count()) {
+                add(Events(
+                    id = it.toString(),
+                    title = "The Definitive Guide to an Uncertain World",
+                    category = "WORKSHOP",
+                    image = "",
+                    toDate = "12 - 15 Nov, 20",
+                ))
+            }
 
 
-
+        }
 }
