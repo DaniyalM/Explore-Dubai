0dda9e00658d00fe161dbe59162376123f70df14
zbook <daniyal.moeen@express.com.pk>
2021-03-11 17:30:44 -0800

    Request has been handled

--- app/src/main/java/com/app/dubaiculture/data/repository/event/local/models/EventHomeListing.kt
+++ app/src/main/java/com/app/dubaiculture/data/repository/event/local/models/EventHomeListing.kt
@@ -2,8 +2,7 @@ package com.app.dubaiculture.data.repository.event.local.models
 
 
 data class EventHomeListing(
-    var title: String? = null,
-    var category: String? = null,
+    var featureEvents:ArrayList<Events>,
     var events: List<Events>,
 )
 
