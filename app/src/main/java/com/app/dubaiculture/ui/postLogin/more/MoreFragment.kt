c76b304f1e759b06e9c30e71e94395815d2fb9b6
m__ammar <m.ammar.appxone@gmail.com>
2021-05-18 14:25:31 +0500

    More Screen almost Done

--- app/src/main/java/com/app/dubaiculture/ui/postLogin/more/MoreFragment.kt
+++ app/src/main/java/com/app/dubaiculture/ui/postLogin/more/MoreFragment.kt
@@ -5,21 +5,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.app.dubaiculture.R
 import com.app.dubaiculture.databinding.FragmentMoreBinding
+import com.app.dubaiculture.databinding.ItemsMoreLayoutBinding
 import com.app.dubaiculture.ui.base.BaseFragment
-import com.app.dubaiculture.ui.postLogin.explore.viewmodel.ExploreMapViewModel
+import com.app.dubaiculture.ui.postLogin.events.`interface`.RowClickListener
+import com.app.dubaiculture.ui.postLogin.more.adapter.MoreItems
 import com.app.dubaiculture.ui.postLogin.more.viewmodel.MoreViewModel
 import com.google.android.material.shape.CornerFamily
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.plan_a_trip_layout.view.*
-import kotlinx.android.synthetic.main.toolbar_layout.*
 import kotlinx.android.synthetic.main.toolbar_layout.view.*
 
 @AndroidEntryPoint
-class MoreFragment : BaseFragment<FragmentMoreBinding>() {
+class MoreFragment : BaseFragment<FragmentMoreBinding>(), View.OnClickListener {
     private val moreViewModel: MoreViewModel by viewModels()
-
+    lateinit var newsAdapter: GroupAdapter<GroupieViewHolder>
+    lateinit var settingAdapter: GroupAdapter<GroupieViewHolder>
     override fun getFragmentBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
@@ -28,9 +33,16 @@ class MoreFragment : BaseFragment<FragmentMoreBinding>() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         subscribeUiEvents(moreViewModel)
-        moreViewModel.setupToolbarWithSearchItems(binding.root.profilePic,binding.root.img_drawer,binding.root.toolbar_title,resources.getString(R.string.more))
+        moreViewModel.setupToolbarWithSearchItems(
+            binding.root.profilePic,
+            binding.root.img_drawer,
+            binding.root.toolbar_title,
+            resources.getString(R.string.more)
+        )
+        rvSetUp()
         cardViewRTL()
     }
+
     private fun cardViewRTL() {
         val radius = resources.getDimension(R.dimen.my_corner_radius_plan)
         binding.root.apply {
@@ -51,6 +63,57 @@ class MoreFragment : BaseFragment<FragmentMoreBinding>() {
                         .setBottomRightCornerSize(radius)
                         .build()
             }
+        }
+    }
+
+    private fun rvSetUp() {
+        newsAdapter = GroupAdapter()
+        settingAdapter = GroupAdapter()
+        moreViewModel.servicesList().map {
+            groupAdapter.add(MoreItems<ItemsMoreLayoutBinding>(
+                object : RowClickListener{
+                    override fun rowClickListener(position: Int) {
+
+                    }
+                },moreModel = it , resLayout = R.layout.items_more_layout,requireContext()))
+        }
+        binding.rvServices.apply {
+            isNestedScrollingEnabled = false
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            adapter = groupAdapter
+        }
+        moreViewModel.newsList().map {
+            newsAdapter.add(MoreItems<ItemsMoreLayoutBinding>(
+                object : RowClickListener{
+                    override fun rowClickListener(position: Int) {
+
+                    }
+                },moreModel = it , resLayout = R.layout.items_more_layout,requireContext()))
+        }
+        binding.rvNews.apply {
+            isNestedScrollingEnabled = false
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            adapter = newsAdapter
+        }
+        moreViewModel.settingsList().map {
+            settingAdapter.add(MoreItems<ItemsMoreLayoutBinding>(
+                object : RowClickListener{
+                    override fun rowClickListener(position: Int) {
+
+                    }
+                },moreModel = it , resLayout = R.layout.items_more_layout,requireContext()))
+        }
+        binding.rvSettings.apply {
+            isNestedScrollingEnabled = false
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            adapter = settingAdapter
+        }
+    }
+
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+
         }
     }
 }
--- app/src/main/java/com/app/dubaiculture/ui/postLogin/more/viewmodel/MoreViewModel.kt
+++ app/src/main/java/com/app/dubaiculture/ui/postLogin/more/viewmodel/MoreViewModel.kt
@@ -6,17 +6,165 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.hilt.lifecycle.ViewModelInject
 import com.app.dubaiculture.R
+import com.app.dubaiculture.data.repository.more.MoreModel
 import com.app.dubaiculture.ui.base.BaseViewModel
-import kotlinx.android.synthetic.main.toolbar_layout.view.*
 
-class MoreViewModel @ViewModelInject constructor(application: Application):BaseViewModel(application){
+class MoreViewModel @ViewModelInject constructor(application: Application) :
+    BaseViewModel(application) {
 
 
-
-    fun setupToolbarWithSearchItems(logoImg : ImageView , pin :ImageView , tvTitle : TextView , heading : String) {
+    fun setupToolbarWithSearchItems(
+        logoImg: ImageView,
+        pin: ImageView,
+        tvTitle: TextView,
+        heading: String
+    ) {
         logoImg.visibility = View.GONE
         pin.visibility = View.GONE
         tvTitle.visibility = View.VISIBLE
         tvTitle.text = heading
-            }
-        }
+    }
+
+
+    fun servicesList(): ArrayList<MoreModel> {
+        val list = ArrayList<MoreModel>()
+        list.add(
+            MoreModel(
+                R.drawable.museum_more,
+                getApplication<Application>().getString(R.string.museum_services),
+                R.drawable.forward_arrow,
+                true
+            )
+        )
+        list.add(
+            MoreModel(
+                R.drawable.public_more,
+                getApplication<Application>().getString(R.string.public_libraries_services),
+                R.drawable.forward_arrow,
+                true
+            )
+        )
+        list.add(
+            MoreModel(
+                R.drawable.renting_more,
+                getApplication<Application>().getString(R.string.renting_more),
+                R.drawable.forward_arrow,
+                true
+            )
+        )
+        list.add(
+            MoreModel(
+                R.drawable.general_more,
+                getApplication<Application>().getString(R.string.general_more),
+                R.drawable.forward_arrow,
+                true
+            )
+        )
+        list.add(
+            MoreModel(
+                R.drawable.heritage_more,
+                getApplication<Application>().getString(R.string.heritage_more),
+                R.drawable.forward_arrow,
+                true
+            )
+        )
+
+        return list
+    }
+
+    fun newsList(): ArrayList<MoreModel> {
+        val list = ArrayList<MoreModel>()
+        list.add(
+            MoreModel(
+                R.drawable.museum_more,
+                getApplication<Application>().getString(R.string.news_more),
+                R.drawable.forward_arrow,
+                false
+            )
+        )
+        list.add(
+            MoreModel(
+                R.drawable.mob_register,
+                getApplication<Application>().getString(R.string.contact_more),
+                R.drawable.forward_arrow,
+                false
+            )
+        )
+        list.add(
+            MoreModel(
+                R.drawable.renting_more,
+                getApplication<Application>().getString(R.string.help_more),
+                R.drawable.forward_arrow,
+                false
+            )
+        )
+        list.add(
+            MoreModel(
+                R.drawable.mob_register,
+                getApplication<Application>().getString(R.string.related_more),
+                R.drawable.forward_arrow,
+                false
+            )
+        )
+        list.add(
+            MoreModel(
+                R.drawable.privacy_more,
+                getApplication<Application>().getString(R.string.privacy_more),
+                R.drawable.forward_arrow,
+                false
+            )
+        )
+        list.add(
+            MoreModel(
+                R.drawable.privacy_more,
+                getApplication<Application>().getString(R.string.terms_and_conditions),
+                R.drawable.forward_arrow,
+                false
+            )
+        )
+
+        return list
+    }
+
+
+
+    fun settingsList(): ArrayList<MoreModel> {
+        val list = ArrayList<MoreModel>()
+        list.add(
+            MoreModel(
+                R.drawable.museum_more,
+                getApplication<Application>().getString(R.string.setting_more),
+                R.drawable.forward_arrow,
+                false
+            )
+        )
+        list.add(
+            MoreModel(
+                R.drawable.mob_register,
+                getApplication<Application>().getString(R.string.accessiblitiy_more),
+                R.drawable.forward_arrow,
+                false
+            )
+        )
+        list.add(
+            MoreModel(
+                R.drawable.renting_more,
+                getApplication<Application>().getString(R.string.switch_language),
+                R.drawable.forward_arrow,
+                false
+            )
+        )
+        list.add(
+            MoreModel(
+                R.drawable.mob_register,
+                getApplication<Application>().getString(R.string.logout_more),
+                R.drawable.forward_arrow,
+                false
+            )
+        )
+
+        return list
+    }
+
+
+}
