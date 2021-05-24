7cd0a478cdbbdc2bb2c8a1996632adb06ca0d513
m__ammar <m.ammar.appxone@gmail.com>
2021-05-24 12:10:28 +0500

    setup of latest news RV

--- app/src/main/java/com/app/dubaiculture/ui/postLogin/more/news/LatestNewsFragment.kt
+++ app/src/main/java/com/app/dubaiculture/ui/postLogin/more/news/LatestNewsFragment.kt
@@ -6,8 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.app.dubaiculture.R
 import com.app.dubaiculture.databinding.FragmentLatestNewsBinding
+import com.app.dubaiculture.databinding.ItemsLatestNewsBinding
 import com.app.dubaiculture.ui.base.BaseFragment
+import com.app.dubaiculture.ui.postLogin.events.`interface`.RowClickListener
+import com.app.dubaiculture.ui.postLogin.more.news.adapter.NewsItems
 import com.app.dubaiculture.ui.postLogin.more.news.viewmodel.NewsViewModel
 
 class LatestNewsFragment : BaseFragment<FragmentLatestNewsBinding>(), View.OnClickListener {
@@ -30,10 +34,18 @@ class LatestNewsFragment : BaseFragment<FragmentLatestNewsBinding>(), View.OnCli
     }
 
     private fun rvSetup(){
-    binding.rvHorizontalNews.apply {
-        layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-        adapter = groupAdapter
-    }
+        newsViewModel.newsList().map {
+            groupAdapter.add(NewsItems<ItemsLatestNewsBinding>(
+                object : RowClickListener{
+                    override fun rowClickListener(position: Int) {
 
+                    }
+
+                },latestNews = it, R.layout.items_latest_news,requireContext()))
+        }
+        binding.rvHorizontalNews.apply {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+            adapter = groupAdapter
+        }
     }
 }
