7286f07d2323537c904f913c4f784f78491b4a3b
m__ammar <m.ammar.appxone@gmail.com>
2021-08-25 10:43:45 +0500

    register event view Model added

---
+++ app/src/main/java/com/app/dubaiculture/ui/postLogin/events/detail/registernow/viewmodel/RegisterNowViewModel.kt
@@ -0,0 +1,9 @@
+package com.app.dubaiculture.ui.postLogin.events.detail.registernow.viewmodel
+
+import android.app.Application
+import com.app.dubaiculture.ui.base.BaseViewModel
+
+class RegisterNowViewModel(application: Application) : BaseViewModel(application) {
+
+
+}
