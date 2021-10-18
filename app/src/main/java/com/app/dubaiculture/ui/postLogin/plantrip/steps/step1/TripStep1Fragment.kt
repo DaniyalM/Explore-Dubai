b0b73dc9e8bc4d09cf20b7514d3781e1434cecfd
AzureAD\AbbasHussain <abbas.ali@greenlightlabs.tech>
2021-10-18 16:59:35 +0500

    Added ViewModel in trip Step1

--- app/src/main/java/com/app/dubaiculture/ui/postLogin/plantrip/steps/step1/TripStep1Fragment.kt
+++ app/src/main/java/com/app/dubaiculture/ui/postLogin/plantrip/steps/step1/TripStep1Fragment.kt
@@ -4,15 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.app.dubaiculture.R
 import com.app.dubaiculture.databinding.FragmentTripStep1Binding
 import com.app.dubaiculture.ui.base.BaseFragment
+import com.app.dubaiculture.ui.postLogin.more.profile.viewmodels.ProfileViewModel
 import com.app.dubaiculture.ui.postLogin.plantrip.callback.CustomNavigation
 import com.app.dubaiculture.ui.postLogin.plantrip.steps.PlanTripParentFragment
+import com.app.dubaiculture.ui.postLogin.plantrip.viewmodels.Step1ViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class TripStep1Fragment : BaseFragment<FragmentTripStep1Binding>() {
+
+    private val step1ViewModel: Step1ViewModel by viewModels()
 
     companion object{
         lateinit var customNavigation:CustomNavigation
@@ -28,7 +33,17 @@ class TripStep1Fragment : BaseFragment<FragmentTripStep1Binding>() {
         super.onViewCreated(view, savedInstanceState)
         binding.view = this
         lottieAnimationRTL(binding.animationView)
+        subscribeToObservables()
 
+
+    }
+
+    private fun subscribeToObservables() {
+        step1ViewModel.userType.observe(viewLifecycleOwner){
+
+            it.
+
+        }
     }
 
     override fun onResume() {
