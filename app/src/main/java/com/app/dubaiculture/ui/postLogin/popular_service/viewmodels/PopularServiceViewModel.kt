4875fddc9bef6ae8610cea0d357ec38b36686585
dmoeen <daniyal.moeen@greenlightlabs.tech>
2021-10-22 22:12:16 +0500

    https://teamcollective.atlassian.net/browse/DCMA01-402 https://teamcollective.atlassian.net/browse/DCMA01-410 https://teamcollective.atlassian.net/browse/DCMA01-314 https://teamcollective.atlassian.net/browse/DCMA01-344 https://teamcollective.atlassian.net/browse/DCMA01-349

--- app/src/main/java/com/app/dubaiculture/ui/postLogin/popular_service/viewmodels/PopularServiceViewModel.kt
+++ app/src/main/java/com/app/dubaiculture/ui/postLogin/popular_service/viewmodels/PopularServiceViewModel.kt
@@ -1,27 +1,17 @@
 package com.app.dubaiculture.ui.postLogin.popular_service.viewmodels
 
 import android.app.Application
-import android.content.Context
-import android.view.View
-import android.widget.EditText
-import android.widget.ImageView
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import com.app.dubaiculture.R
 import com.app.dubaiculture.data.Result
 import com.app.dubaiculture.data.repository.popular_service.ServiceRepository
 import com.app.dubaiculture.data.repository.popular_service.local.models.EService
-import com.app.dubaiculture.data.repository.popular_service.local.models.EServices
 import com.app.dubaiculture.data.repository.popular_service.local.models.ServiceCategory
 import com.app.dubaiculture.data.repository.popular_service.remote.request.EServiceRequest
-import com.app.dubaiculture.databinding.ItemsServiceListingLayoutBinding
 import com.app.dubaiculture.infrastructure.ApplicationEntry
 import com.app.dubaiculture.ui.base.BaseViewModel
-import com.app.dubaiculture.ui.postLogin.popular_service.adapter.PopularServiceListItem
 import com.app.dubaiculture.utils.event.Event
-import com.xwray.groupie.GroupAdapter
-import com.xwray.groupie.GroupieViewHolder
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -34,8 +24,8 @@ class PopularServiceViewModel @Inject constructor(
 
     var id: String = ""
 
-    private val _eServices: MutableLiveData<Result<EServices>> = MutableLiveData()
-    val eServices: LiveData<Result<EServices>> = _eServices
+//    private val _eServices: MutableLiveData<Result<EServices>> = MutableLiveData()
+//    val eServices: LiveData<Result<EServices>> = _eServices
 
     private val _serviceList: MutableLiveData<List<EService>> = MutableLiveData()
     private val _serviceListTemp: MutableLiveData<List<EService>> = MutableLiveData()
@@ -59,7 +49,7 @@ class PopularServiceViewModel @Inject constructor(
             when (val result = serviceRepository.getEServices(EServiceRequest(culture = locale))) {
                 is Result.Success -> {
                     showLoader(false)
-                    _eServices.value = result
+//                    _eServices.value = result
                     _serviceListCategory.value = Event(result.value.serviceCategory)
                     _serviceList.value = result.value.eServices
                     _serviceListTemp.value = _serviceList.value
@@ -68,7 +58,7 @@ class PopularServiceViewModel @Inject constructor(
                 }
                 is Result.Failure -> {
                     showLoader(false)
-                    _eServices.value = result
+//                    _eServices.value = result
                 }
 
             }
@@ -117,42 +107,42 @@ class PopularServiceViewModel @Inject constructor(
         return getFilterData as MutableList<EService>
     }
 
-    fun returnSearchList(
-        searchList: ArrayList<EService>,
-        editTextKey: EditText,
-        adapter: GroupAdapter<GroupieViewHolder>,
-        imgCancelBtn: ImageView,
-        context: Context
-    ) {
-        adapter.clear()
-        val myFilterSearchList = searchList.filter {
-            it.title.contains(editTextKey.text.toString().trim(), ignoreCase = true)
-        }
-        if (editTextKey.text.toString().trim().isNullOrEmpty()) {
-            imgCancelBtn.visibility = View.GONE
-            searchList.map {
-                adapter.add(
-                    PopularServiceListItem<ItemsServiceListingLayoutBinding>(
-                        eService = it,
-                        resLayout = R.layout.items_service_listing_layout,
-                        context = context
-
-                    )
-                )
-            }
-        } else {
-            imgCancelBtn.visibility = View.VISIBLE
-            myFilterSearchList.map {
-                adapter.add(
-                    PopularServiceListItem<ItemsServiceListingLayoutBinding>(
-                        eService = it,
-                        resLayout = R.layout.items_service_listing_layout,
-                        context = context
-                    )
-                )
-            }
-        }
-        adapter.notifyDataSetChanged()
-
-    }
+//    fun returnSearchList(
+//        searchList: ArrayList<EService>,
+//        editTextKey: EditText,
+//        adapter: GroupAdapter<GroupieViewHolder>,
+//        imgCancelBtn: ImageView,
+//        context: Context
+//    ) {
+//        adapter.clear()
+//        val myFilterSearchList = searchList.filter {
+//            it.title.contains(editTextKey.text.toString().trim(), ignoreCase = true)
+//        }
+//        if (editTextKey.text.toString().trim().isNullOrEmpty()) {
+//            imgCancelBtn.visibility = View.GONE
+//            searchList.map {
+//                adapter.add(
+//                    PopularServiceListItem<ItemsServiceListingLayoutBinding>(
+//                        eService = it,
+//                        resLayout = R.layout.items_service_listing_layout,
+//                        context = context
+//
+//                    )
+//                )
+//            }
+//        } else {
+//            imgCancelBtn.visibility = View.VISIBLE
+//            myFilterSearchList.map {
+//                adapter.add(
+//                    PopularServiceListItem<ItemsServiceListingLayoutBinding>(
+//                        eService = it,
+//                        resLayout = R.layout.items_service_listing_layout,
+//                        context = context
+//                    )
+//                )
+//            }
+//        }
+//        adapter.notifyDataSetChanged()
+//
+//    }
 }
