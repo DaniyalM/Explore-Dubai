760f7bd04c79606f58163f6ac5bb061130d02d80
dmoeen <daniyal.moeen@greenlightlabs.tech>
2021-10-27 19:51:57 +0500

    Merge branch 'dc-sp4-gs' of https://dev.azure.com/TrafficOnline/Dubai%20Culture%20Android%20App/_git/Dubai%20Culture%20Android%20App into dc-sprint-5
    # Conflicts:
    #	app/src/main/java/com/app/dubaiculture/ui/postLogin/home/HomeFragment.kt
    #	app/src/main/res/navigation/more_navigation.xml

--- app/src/main/java/com/app/dubaiculture/ui/preLogin/viewmodel/PreLoginViewModel.kt
+++ app/src/main/java/com/app/dubaiculture/ui/preLogin/viewmodel/PreLoginViewModel.kt
@@ -24,7 +24,7 @@ class PreLoginViewModel @Inject constructor(
 
         viewModelScope.launch {
             userRepository.getLastUser()?.let {
-                setUser(it, true)
+                setUser(it)
             }
         }
     }
