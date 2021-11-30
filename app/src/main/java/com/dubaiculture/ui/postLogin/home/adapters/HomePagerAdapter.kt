f7fd80db0512462baf59d4cf63fa024266defee5
dmoeen <daniyal.moeen@greenlightlabs.tech>
2021-11-30 17:21:45 +0500

    Attraction

--- app/src/main/java/com/dubaiculture/ui/postLogin/home/adapters/HomePagerAdapter.kt
+++ app/src/main/java/com/dubaiculture/ui/postLogin/home/adapters/HomePagerAdapter.kt
@@ -2,8 +2,7 @@ package com.dubaiculture.ui.postLogin.home.adapters
 
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.dubaiculture.ui.postLogin.attractions.AttractionsFragment
-import com.dubaiculture.ui.postLogin.attractions.detail.AttractionDetailFragment
+import com.dubaiculture.ui.postLogin.attractions.listing.AttractionFragment
 import com.dubaiculture.ui.postLogin.events.EventFilterFragment
 import com.dubaiculture.ui.postLogin.events.EventsFragment
 import com.dubaiculture.ui.postLogin.explore.ExploreFragment
@@ -20,7 +19,7 @@ class HomePagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
                 EventsFragment()
             }
             2 -> {
-                AttractionsFragment()
+                AttractionFragment()
             }
             else -> {
                 EventFilterFragment()
